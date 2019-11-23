e45a52fcd62855481043534e0024594fd85fa85b
laichao <1531791223@qq.com>
2019-11-23 20:38:35 +0800

    code

---
+++ 多继承/多继承/多继承.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARINGS
+#include<iostream>
+using namespace std;
+
+class Father1
+{
+public:
+	int bmw;
+};
+
+class Father2
+{
+public:
+	int bmw;
+};
+
+class Son :public Father1, public Father2
+{
+
+};
+
+void test()
+{
+	Son s;
+	//s.bmw;有二义性问题
+}
+
+int main()
+{
+	system("pause");
+	return EXIT_SUCCESS;
+}
